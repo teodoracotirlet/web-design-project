b9e9afaaa42678972cf22b14854a8f3d460104fa
Teodora Coîrleț <teodoracotirlet@yahoo.com>
2023-01-02 17:58:53 +0200

    Add files via upload

---
+++ about.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+   
+
+
+</head>
+    <body>
+  
+
+    <div class = "baraprincipala">
+
+    <div class = "logo">
+        <img src= "imaginihomepage/logo.png" width="25%">
+
+    </div>
+    
+    <div class = "linkuriprincipale">
+        
+        <a href= "photographysite.php" class = "homebutton" >Home</a>
+        <a href= "gallery.php" class = "gallerybutton" >Gallery</a>
+        <a href= "account.php" class = "accountbutton" >Account</a>
+        <a href= "shop.php" class = "shopbutton" >Shop</a>
+        <a href= "about.php" class = "aboutbutton" >About</a>
+
+    </div>
+
+    </div><br><br><br><br><br><br>
+
+
+    <div class = "textcentru">
+        <p class = "aboutus">About us</p>
+        <p class = "textbox1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, </p>
+    </div><br>
+
+    <div class="row">
+  <div class="column">
+    <img src="imgcarusel/brasovcraciun.jpeg" alt="Snow" style="width:100%">
+    <p>hfjkhg</p>
+  </div>
+  <div class="column">
+    <img src="imgcarusel/brasovcraciun.jpeg" alt="Forest" style="width:100%">
+    <p>hgskjhg</p>
+  </div>
+  <div class="column">
+    <img src="imgcarusel/brasovcraciun.jpeg" alt="Mountains" style="width:100%">
+    <p>jhfsh</p>
+  </div>
+
+    </body>
+</html>
