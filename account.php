718f0dc71cd8fddb2f3574b7013a3be78cfbc71d
Teodora Coîrleț <teodoracotirlet@yahoo.com>
2023-01-03 19:01:25 +0200

    Update account.php

--- account.php
+++ account.php
@@ -9,8 +9,8 @@
 
 </head>
     <body>
-  
 
+    
     <div class = "baraprincipala">
 
     <div class = "logo">
@@ -25,59 +25,22 @@
         <a href= "account.php" class = "accountbutton" >Account</a>
         <a href= "shop.php" class = "shopbutton" >Shop</a>
         <a href= "about.php" class = "aboutbutton" >About</a>
-        
-
-
-    </div>
 
     </div><br><br><br><br><br>
 
-    
-<h2>Modal Login Form</h2>
-
-<button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Login</button>
-
-<div id="id01" class="modal">
   
-  <form class="modal-content animate" action="/action_page.php" method="post">
-    <div class="imgcontainer">
-      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-      <img src="img_avatar2.png" alt="Avatar" class="avatar">
-    </div>
+  
+<h1>Create your account</h1>
 
-    <div class="container">
-      <label for="uname"><b>Username</b></label>
-      <input type="text" placeholder="Enter Username" name="uname" required>
-
-      <label for="psw"><b>Password</b></label>
-      <input type="password" placeholder="Enter Password" name="psw" required>
-        
-      <button type="submit">Login</button>
-      <label>
-        <input type="checkbox" checked="checked" name="remember"> Remember me
-      </label>
-    </div>
-
-    <div class="container" style="background-color:#f1f1f1">
-      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-      <span class="psw">Forgot <a href="#">password?</a></span>
-    </div>
-  </form>
-</div>
-
-<script>
-// Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-</script>
-
-
+<form action="accountselection.php" method="POST">
+   First Name: <input type="text1" name="firstname" required><br> 
+   Last Name: <input type="text1" name="lastname" required><br>
+   Email: <input type="text1" name="email" required><br>
+   Age: <input type="text1" name="age" required><br>
+   
+   <input type="reset" value="Reset">
+   <input type="submit1" value="Send">
+</form>
 
 
 
